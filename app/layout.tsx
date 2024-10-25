b9734a794ce2a351aef3eff843ad9f8528d3a4eb
Polo2230 <johans.polon@autonoma.edu.co>
2024-10-25 17:01:59 -0500

    Arreglando Vercel4

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,11 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// Definición de la interfaz para las propiedades
-interface RootLayoutProps {
-  children: React.ReactNode;
-  metadata?: Metadata; // metadata puede ser opcional
-}
-
-export default function RootLayout({ children, metadata }: RootLayoutProps) {
+// Cambiamos la declaración del componente para usar React.FC
+const RootLayout: React.FC<{ children: React.ReactNode; metadata?: Metadata }> = ({
+  children,
+  metadata,
+}) => {
   const defaultMetadata: Metadata = {
     title: "Home of Party",
     description: "Welcome to the best place to find the best parties and events.",
@@ -22,10 +20,13 @@ export default function RootLayout({ children, metadata }: RootLayoutProps) {
   return (
     <html lang="en">
       <head>
-        <title dangerouslySetInnerHTML={{ __html: finalMetadata.title || "Default Title" }}></title>
-        <meta name="description" content={finalMetadata.description || ""} />
+        <title>{String(finalMetadata.title) ?? "Default Title"}</title>
+        <meta name="description" content={finalMetadata.description ?? "Default Description"} />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout; // Asegúrate de que la exportación sea correcta
+
